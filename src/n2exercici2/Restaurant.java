b9ad68ff802e5c781798541346ce631f74e51660
Esther Arias <eariasvalor@gmail.com>
2025-10-01 13:01:28 +0200

    feat: add Comparable implementation, override compareTo()

---
+++ src/n2exercici2/Restaurant.java
@@ -0,0 +1,56 @@
+package n2exercici2;
+
+import java.util.Objects;
+
+public class Restaurant implements Comparable<Restaurant> {
+    private String name;
+    private int score;
+
+    public Restaurant(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getScore(){
+        return this.score;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String toString(){
+        return this.name + ", puntuació: " + this.score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Restaurant that = (Restaurant) o;
+        return score == that.score && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, score);
+    }
+
+    @Override
+    public int compareTo(Restaurant r) {
+        int result = this.name.compareTo(r.name);
+
+        if (result == 0) {
+            result = Integer.compare(r.score, this.score);
+        }
+
+        return result;
+    }
+}
