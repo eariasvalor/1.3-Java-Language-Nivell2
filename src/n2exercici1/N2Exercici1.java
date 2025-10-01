ccc148d3afa1cfc006f2b34c68bc945fbb9af7dc
Esther Arias <eariasvalor@gmail.com>
2025-10-01 11:27:11 +0200

    feat: create HashSet, added objects and printed with a for

--- src/n2exercici1/N2Exercici1.java
+++ src/n2exercici1/N2Exercici1.java
@@ -1,4 +1,17 @@
 package n2exercici1;
 
+import java.util.HashSet;
+
 public class N2Exercici1 {
+    public static void main (String[] args){
+        HashSet<Restaurant> restaurants = new HashSet<>();
+
+        restaurants.add(new Restaurant("Casa Pepe", 3));
+        restaurants.add(new Restaurant("Casa Pepe", 4));
+        restaurants.add(new Restaurant("Casa Pepe", 3));
+
+        for(Restaurant r : restaurants) {
+            System.out.println(r.toString());
+        }
+    }
 }
