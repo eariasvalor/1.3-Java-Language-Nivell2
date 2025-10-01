ac09da58f65a7fbcceb3ecc91927960b88f61fcd
Esther Arias <eariasvalor@gmail.com>
2025-10-01 11:26:37 +0200

    feat: create attributes, getters/setters and overrode equals and hashcode methods

--- src/n2exercici1/Restaurant.java
+++ src/n2exercici1/Restaurant.java
@@ -1,4 +1,45 @@
 package n2exercici1;
 
+import java.util.Objects;
+
 public class Restaurant {
+    private String name;
+    private int score;
+
+    public Restaurant(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getScore(){
+        return this.score;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String toString(){
+        return this.name + ", puntuació: " + this.score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Restaurant that = (Restaurant) o;
+        return score == that.score && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, score);
+    }
 }
